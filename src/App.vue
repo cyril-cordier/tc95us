b547cdce946893779725da3fc7b1948f0b183956
Cyril <cyril.cordier@epitech.eu>
2021-05-09 15:20:36 +0200

    chgt google tag analysis

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,91 @@
 <template>
   <div id="app">
-    <div id="barnav">
+    <div class="barnav">
       <div id="nav">
+        <nav class="navbar nav-tabs navbar-expand-lg navbar-light">
+          <router-link to="/">
+            <div @click="refresh()">
+              <img
+                alt="Vue logo"
+                src="./assets/logo_tcus.png"
+                style="height: 5rem"
+              />
+            </div>
+          </router-link>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarText"
+            aria-controls="navbarText"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <router-link to="/" class="nav-link">
+                  <div @click="refresh()">Accueil</div>
+                </router-link>
+              </li>
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  data-toggle="dropdown"
+                  href="#"
+                  role="button"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                  >Le Club</a
+                >
+                <div class="dropdown-menu">
+                  <router-link to="/club" class="dropdown-item"
+                    >Nos installations</router-link
+                  >
+                  <router-link to="/carte" class="dropdown-item"
+                    >Nous trouver</router-link
+                  >
+                  <router-link to="/evenements" class="dropdown-item"
+                    >Animations et Evènements</router-link
+                  >
+                  <router-link to="/tarifs" class="dropdown-item"
+                    >Tarifs</router-link
+                  >
+                  <router-link to="/bureau" class="dropdown-item"
+                    >Les membres du bureau</router-link
+                  >
+                  <router-link to="/reglement_club" class="dropdown-item"
+                    >Règlement du Club</router-link
+                  >
+                  <!-- <div class="dropdown-divider"></div> -->
+                </div>
+              </li>
 
-      
-      <nav class="navbar nav-tabs navbar-expand-lg navbar-light" >
-        <router-link to="/">
-          <div @click="refresh()"><img alt="Vue logo" src="./assets/logo_tcus.png" style="height:5rem"></div>
-        </router-link>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText"
-          aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarText">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <router-link to="/" class="nav-link ">
-                <div @click="refresh()">Accueil</div>
-              </router-link>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true"
-                aria-expanded="false">Le Club</a>
-              <div class="dropdown-menu">
-                <router-link to="/club" class="dropdown-item">Nos installations</router-link>
-                <router-link to="/carte" class="dropdown-item">Nous trouver</router-link>
-                <router-link to="/evenements" class="dropdown-item">Animations et Evènements</router-link>
-                <router-link to="/tarifs" class="dropdown-item">Tarifs</router-link>
-                <router-link to="/bureau" class="dropdown-item">Les membres du bureau</router-link>
-                <router-link to="/reglement_club" class="dropdown-item">Règlement du Club</router-link>
-                <!-- <div class="dropdown-divider"></div> -->
-              </div>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true"
-                aria-expanded="false">Compétitions</a>
-              <div class="dropdown-menu">
-                <router-link to="/championnats" class="dropdown-item">Les Championnats par équipe</router-link>
-                <router-link to="/challenge" class="dropdown-item">Le Challenge</router-link>
-              </div>
-            </li>
-          </ul>
-
-        </div>
-      </nav>
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  data-toggle="dropdown"
+                  href="#"
+                  role="button"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                  >Compétitions</a
+                >
+                <div class="dropdown-menu">
+                  <router-link to="/championnats" class="dropdown-item"
+                    >Les Championnats par équipe</router-link
+                  >
+                  <router-link to="/challenge" class="dropdown-item"
+                    >Le Challenge</router-link
+                  >
+                </div>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </div>
-
-
 
       <!-- <div id="nav">
         <ul class="nav nav-tabs">
@@ -86,7 +126,6 @@
       <router-view class="contenu" />
     </div>
 
-
     <!-- <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true"
             aria-expanded="false">Mon compte</a>
@@ -104,37 +143,34 @@
     <!-- </div>
         </li> -->
 
-
-
-
-
-
-
-
-
-
     <footer>
       <div class="container-fluid">
         <div class="row">
-
           <div class="partenaires">
             Nos partenaires
-            <img class="logopart" id="logo ecosport" alt="Vue logo" src="./assets/ecosport_small.jpg">
-            <img class="logopart" alt="logo technifibre" src="./assets/technifibre_small.jpg">
-
+            <img
+              class="logopart"
+              id="logo ecosport"
+              alt="Vue logo"
+              src="./assets/ecosport_small.jpg"
+            />
+            <img
+              class="logopart"
+              alt="logo technifibre"
+              src="./assets/technifibre_small.jpg"
+            />
           </div>
-
-
         </div>
         <div class="card-group">
-
           <div class="card">
             <div class="card-body">
               <h5 class="card-title">Contact</h5>
-              <p class="card-text"><strong>Tennis Club de Us</strong><br>
-                jardin public (RD 28)<br>
-                95450 Us<br>
-                <a href="mailto:tc95us@gmail.com">tc95us@gmail.com</a></p>
+              <p class="card-text">
+                <strong>Tennis Club de Us</strong><br />
+                jardin public (RD 28)<br />
+                95450 Us<br />
+                <a href="mailto:tc95us@gmail.com">tc95us@gmail.com</a>
+              </p>
 
               <p class="card-text">
                 <router-link to="/carte">
@@ -160,18 +196,31 @@
           </div>
           <div class="card">
             <div class="card-body">
-              <img alt="logo technifibre" src="./assets/logoclubaffilie.png" style="height:5rem">
+              <img
+                alt="logo technifibre"
+                src="./assets/logoclubaffilie.png"
+                style="height: 5rem"
+              />
             </div>
           </div>
           <div class="card">
             <div class="card-body">
               <h5 class="card-title">Vos liens utiles</h5>
-              <p class="card-text"><a href="https://tenup.fft.fr/" target="blank">Mon espace Tennis</a></p>
-              <p class="card-text"><a href="https://www.fft.fr/" target="blank">Site de la FFT</a> </p>
-              <p class="card-text"><a
+              <p class="card-text">
+                <a href="https://tenup.fft.fr/" target="blank"
+                  >Mon espace Tennis</a
+                >
+              </p>
+              <p class="card-text">
+                <a href="https://www.fft.fr/" target="blank">Site de la FFT</a>
+              </p>
+              <p class="card-text">
+                <a
                   href="https://comite.fft.fr/val-d-oise/val-d-oise_a/cms/index_public.php?PHPSESSID=1c5b3471354cf4e0f185c548eaebf509&us_action=show_note_site&login_off=1&ui_id_site=1"
-                  target="blank">Le site du Comité du Val d'Oise</a></p>
-
+                  target="blank"
+                  >Le site du Comité du Val d'Oise</a
+                >
+              </p>
             </div>
           </div>
           <div class="card">
@@ -179,8 +228,12 @@
               <strong>
                 <h5 class="card-title">Restez connectés</h5>
               </strong>
-              <p class="card-text"><a href="https://www.facebook.com/groups/406488439536879/"
-                  target="blank">Facebook</a>
+              <p class="card-text">
+                <a
+                  href="https://www.facebook.com/groups/406488439536879/"
+                  target="blank"
+                  >Facebook</a
+                >
               </p>
               <p class="card-text">
                 <router-link to="/calendrier_events">
@@ -194,132 +247,123 @@
     </footer>
 
     <div class="footer">
-      Site créé par Cyril CORDIER - <router-link to="/admin">
+      Site créé par Cyril CORDIER -
+      <router-link to="/admin">
         <div class="footer">Accès Admin</div>
       </router-link>
     </div>
-
   </div>
 </template>
 
 <script>
-  import {
-    mapActions,
-    mapGetters
-  } from 'vuex'
+import { mapActions, mapGetters } from "vuex";
 
-  export default {
-
-
-    data() {
-      return {
-
-        users_toggle: true,
-        products_toggle: false,
-        events_toggle: false,
-        challengeusers_toggle: false,
-        challengeresults_toggle: false,
-        championships_toggle: false,
-        tarifs_toggle: false,
-        contents_toggle: false,
-        officeusers_toggle: false,
-        id: '',
-        lastname: '',
-        firstname: '',
-        admin: '',
-      }
-    },
-    methods: {
-
-      ...mapActions(['fetchUser', 'fetchUserById']),
-      /* logout() {
+export default {
+  data() {
+    return {
+      users_toggle: true,
+      products_toggle: false,
+      events_toggle: false,
+      challengeusers_toggle: false,
+      challengeresults_toggle: false,
+      championships_toggle: false,
+      tarifs_toggle: false,
+      contents_toggle: false,
+      officeusers_toggle: false,
+      id: "",
+      lastname: "",
+      firstname: "",
+      admin: "",
+    };
+  },
+  methods: {
+    ...mapActions(["fetchUser", "fetchUserById"]),
+    /* logout() {
         window.localStorage.removeItem('token');
         window.location.href = "/";
       },  */
-      refresh() {
-        window.location.href = "/";
-      },
-      refreshiti() {
-        window.location.href = "/carte";
-      },
-      refreshcal() {
-        window.location.href = "/calendrier_events";
-      },
-      refreshrgt() {
-        window.location.href = "/reglement_club";
-      },
-      refreshleg() {
-        window.location.href = "/legal";
-      }
-
+    refresh() {
+      window.location.href = "/";
     },
-    computed: mapGetters(['getMessage', 'getAllUsers', 'getUser', 'infoUserById', ]),
-    created() {
-      this.fetchUser();
-    }
-  }
+    refreshiti() {
+      window.location.href = "/carte";
+    },
+    refreshcal() {
+      window.location.href = "/calendrier_events";
+    },
+    refreshrgt() {
+      window.location.href = "/reglement_club";
+    },
+    refreshleg() {
+      window.location.href = "/legal";
+    },
+  },
+  computed: mapGetters([
+    "getMessage",
+    "getAllUsers",
+    "getUser",
+    "infoUserById",
+  ]),
+  created() {
+    this.fetchUser();
+  },
+};
 </script>
 
 
 <style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-  }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-  #nav {
-    padding: 30px;
-  }
+#nav {
+  padding: 30px;
+}
 
-  #nav a {
-    font-weight: bold;
-    color: #2c3e50;
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
 
-  }
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
 
-  #nav a.router-link-exact-active {
-    color: #42b983;
-  }
+.barnav {
+  background-color: #f0d44a;
+  background-image: linear-gradient(315deg, #f0d44a 0%, #eca794 74%);
+}
 
-  #barnav {
-    background-color: #f0d44a;
-    background-image: linear-gradient(315deg, #f0d44a 0%, #eca794 74%);
+.partenaires {
+  display: block;
+  margin: 50px;
+  margin-left: auto;
+  margin-right: auto;
+}
 
+.grid {
+  grid-auto-rows: minmax(100px, auto);
+}
 
-  }
+.card {
+  border: none;
+  background: transparent;
+}
 
-  .partenaires {
-    display: block;
-    margin: 50px;
-    margin-left: auto;
-    margin-right: auto
-  }
+.logopart {
+  margin: 1rem;
+}
 
-  .grid {
-    grid-auto-rows: minmax(100px, auto);
-  }
+.footer {
+  text-align: center;
+  color: lightgrey;
+}
 
-  .card {
-
-    border: none;
-    background: transparent;
-
-  }
-
-  .logopart {
-
-    margin: 1rem;
-  }
-
-  .footer {
-    text-align: center;
-    color: lightgrey;
-  }
-
-  .contenu {
-    padding: 1.5rem;
-  }
+.contenu {
+  padding: 1.5rem;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,40 +1,27 @@
 <template>
   <div class="home">
-    
-    <img alt="Tc Us" src="../assets/bandeau_tcus.jpg" style="width:100%">
+    <img alt="Tc Us" src="../assets/bandeau_tcus.jpg" style="width: 100%" />
 
-    
-    
-
-    <Accueil msg="Bienvenue sur le site du TC Us"/>
-  
+    <Accueil msg="Bienvenue sur le site du TC Us" />
   </div>
-
-  
 </template>
 
 <script>
 // @ is an alias to /src
-import Accueil from '@/components/accueil.vue'
-
+import Accueil from "@/components/accueil.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    Accueil, 
-    
-  }
-}
-
-  
-
-
+    Accueil,
+  },
+};
 </script>
 
 <style scoped>
-  #logotcus {
-    position: absolute;
-    top: 3%;
-    margin: 32%;
-  }
+#logotcus {
+  position: absolute;
+  top: 3%;
+  margin: 32%;
+}
 </style>
