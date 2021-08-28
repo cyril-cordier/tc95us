9647eb1f3809cca8737b52222b543d49552e3fdf
Cyril <cyril.cordier@epitech.eu>
2021-08-28 18:15:16 +0200

    adaptation 2 colonnes page accueil

--- src/components/accueil.vue
+++ src/components/accueil.vue
@@ -4,7 +4,23 @@
     <h1 class="card-title">{{ msg }}</h1>
     <br /><br />
     <div class="card-deck">
-      <div class="card">
+      <div class="card col-sm-12 col-md-8">
+        <div class="card-body">
+          <h2 class="card-title">Actualités</h2>
+          <div class="my-2" v-for="content in getAllContents" :key="content.id">
+            <div v-if="content.title == 'Texte page Accueil'">
+              <div v-html="content.content"></div>
+
+              <p class="card-text">
+                <small class="text-muted">{{ content.name }}</small>
+              </p>
+              <img :src="content.image" style="width: 15rem" />
+              <hr class="my-4" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="card col-sm-12 col-md-4">
         <div class="card-body">
           <h5 class="card-title">
             <a
@@ -27,25 +43,7 @@
               Tennis Club d&#039;Us
             </blockquote>
           </div>
-        </div>
-      </div>
-      <div class="card">
-        <div class="card-body">
-          <h2 class="card-title">Actualités</h2>
-          <div v-for="content in getAllContents" :key="content.id">
-            <div v-if="content.title == 'Texte page Accueil'">
-              <div v-html="content.content"></div>
-
-              <p class="card-text">
-                <small class="text-muted">{{ content.name }}</small>
-              </p>
-              <img :src="content.image" style="width: 15rem" />
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="card">
-        <div class="card-body">
+          <hr class="my-4" />
           <h5 class="card-title">Quel temps fait-il sur nos courts ?</h5>
           <iframe
             src="https://fr.tutiempo.net/s-widget/full/42145/ca5"
