9f1fd4ae7e975ecff4572480374d6ea44fb44d1e
Cyril Cordier <8158269-cyril.cordier@users.noreply.gitlab.com>
2022-11-27 23:16:32 +0100

    products

--- src/store/modules/products.js
+++ src/store/modules/products.js
@@ -1,169 +1,158 @@
+const axios = require('axios');
 
-
-const token = window.localStorage.getItem('token') || "";
-var url = `https://tc95us.herokuapp.com`;
+const url = process.env.VUE_APP_API_URL + `/classes/products`;
+let headers = {
+  'X-Parse-Application-Id': process.env.VUE_APP_APPLICATION_ID,
+  'X-Parse-REST-API-Key': process.env.VUE_APP_REST_API_KEY,
+  'X-Parse-Session-Token': window.localStorage.getItem('session-token') || "",
+  'X-Parse-Javascript-Key': process.env.VUE_APP_JAVASCRIPT_KEY,
+  'Content-Type': 'application/json'
+}
+if (window.localStorage.getItem('session-token')) {
+  headers['X-Parse-Session-Token'] = window.localStorage.getItem('session-token')
+}
 
 const state = {
-    productmessage:[],
-    updateproductmessage:[],
-    product:[],
-    products:[],
-    ProductById:[],
-    deleteProductById:[],
-
-   
-
+  productmessage:[],
+  updateproductmessage:[],
+  product:[],
+  products:[],
+  ProductById:[],
+  deleteProductById:[],
 }
 
 const getters = {
-    getProductMessage:(state) =>(state.productmessage),
-    getUpdateProductMessage:(state)=>(state.updateproductmessage),
-    getProduct:(state) =>(state.product),
-    infoProductById:(state) =>(state.ProductById),
-    getAllProducts:(state)=> (state.products),
+  getProductMessage:(state) =>(state.productmessage),
+  getUpdateProductMessage:(state)=>(state.updateproductmessage),
+  getProduct:(state) =>(state.product),
+  infoProductById:(state) =>(state.ProductById),
+  getAllProducts:(state)=> (state.products),
 }
 
 const actions = {
-   
-    // CREATE
-    async createProduct({commit},form){
-      var myHeaders = new Headers();
-            myHeaders.append("Authorization", `Bearer ${token}`);
-        var formdata = new FormData();
-        formdata.append("name", form.name);
-        formdata.append("details", form.details);
-        formdata.append("price", form.price);
-        formdata.append("image", form.image);
-        formdata.append("image_name", form.image_name+'.'+form.extension);
-        
-     
-        var requestOptions = {
-          method: 'POST',
-          headers: myHeaders,
-          body: formdata,
-          redirect: 'follow'
-        };
-        
-        fetch(`${url}/api/products`, requestOptions)
-          .then(response => response.json())
-          .then(result =>  {
-              commit('createProductMessage',result);
-              /* location.reload(); */
-             
-            })
-          .catch(error => console.log('error', error));
-    },
-    
-    // EDIT
-    async editProduct({commit},form){
-      var myHeaders = new Headers();
-            myHeaders.append("Authorization", `Bearer ${token}`);
-            myHeaders.append('Content-Type','application/json');
-            var raw=JSON.stringify({
-              "name": form.name,
-              "details": form.details,
-              "price": form.price,
-              "image": form.image,
-              "image_name": form.image_name+'.'+form.extension
-            });
-   
-      var requestOptions = {
-        method: 'PUT',
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow'
-      };
-      
-      fetch(`${url}/api/products/${form.id}`, requestOptions)
-        .then(response => response.json())
-        .then(result =>  {
-            commit('updateProductMessage',result);
-            location.reload();
-           
-          })
-        .catch(error => console.log('error', error));
+
+  // CREATE
+  async createProduct({ commit }, form) {
+
+    const body = {
+      name: form.name,
+      details: form.details,
+      price: form.price,
+      image: form.image,
+      weight: Number(form.weight),
+    }
+
+    var config = {
+      method: 'post',
+      url: `${url}`,
+      headers: headers,
+      data: body
+    };
+
+    axios(config)
+      .then(result => {
+        commit('createProductMessage', result)
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   },
 
-   
-      
-// FETCH BY ID
-    async fetchProductById({commit},id){
-                  var myHeaders = new Headers();
-                  myHeaders.append("Authorization", `Bearer ${token}`);
-      
-                  var requestOptions = {
-                  method: 'GET',
-                  headers: myHeaders,
-                  
-                  redirect: 'follow'
-                  };
-      
-                  fetch(`${url}/api/products/${id}`, requestOptions)
-                  .then(response => response.json())
-                  .then(result => {
-                    commit('ProductById',result);
-    
-                  })
-                  .catch(error => console.log('error', error));
-                      },
-
-    // DELETE BY ID
-    async deleteProduct({commit},id){
-      var myHeaders = new Headers();
-      myHeaders.append("Authorization", `Bearer ${token}`);
-
-      var requestOptions = {
-      method: 'DELETE',
-      headers: myHeaders,
-      
-      redirect: 'follow'
-      };
-
-      fetch(`${url}/api/products/${id}`, requestOptions)
-      .then(response => response.json())
-      .then(result => {
-        commit('deleteProductById',result);
-
-      })
-      .catch(error => console.log('error', error));
-          },
-
-        //FETCH ALL
-     async fetchAllProducts({commit}){
-            var myHeaders = new Headers();
-            myHeaders.append("Authorization", `Bearer ${token}`);
-
-            var requestOptions = {
-            method: 'GET',
-            headers: myHeaders,
-            
-            redirect: 'follow'
-            };
-
-            fetch(`${url}/api/products`, requestOptions)
-            .then(response => response.json())
-            .then(result => commit('fetchAllProducts',result))
-            .catch(error => console.log('error', error));
-                },
-
+  // EDIT
+  async editProduct({ commit }, form) {
+    const body = {
+      name: form.name,
+      details: form.details,
+      price: form.price,
+      image: form.image,
+      weight: Number(form.weight),
     }
-  
 
-const mutations = {
-createProductMessage:(state,productmessage)=>(state.productmessage = productmessage),
-updateProductMessage:(state,updateproductmessage)=>(state.updateproductmessage = updateproductmessage),
-fetchProduct:(state,product) =>(state.product = product),
-fetchAllProducts:(state,products)=>(state.products = products),
-ProductById:(state,ProductById)=>(state.ProductById = ProductById),
-deleteProductById:(state,deleteProductById)=>(state.deleteProductById = deleteProductById),
+    var config = {
+      method: 'put',
+      url: `${url}/${form.objectId}`,
+      headers: headers,
+      data: body
+    };
+
+    axios(config)
+      .then(result => {
+        commit('updateProductMessage', result)
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  },
+
+  // FETCH BY ID
+  async fetchProductById({ commit }, id) {
+    var config = {
+      method: 'get',
+      url: `${url}/${id}`,
+      headers: headers
+    };
+
+    axios(config)
+      .then(response => response.data)
+      .then(result => {
+        commit('ProductById', result)
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  },
+
+  // DELETE BY ID
+  async deleteProduct({ commit }, id) {
+    var config = {
+      method: 'delete',
+      url: `${url}/${id}`,
+      headers: headers
+    };
+
+    axios(config)
+      .then(response => response.data)
+      .then(result => {
+        commit('deleteProductById', result)
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  },
+
+  //FETCH ALL
+  async fetchAllProducts({ commit }) {
+    var config = {
+      method: 'get',
+      url: url,
+      headers: headers
+    };
+
+    axios(config)
+      .then(response => response.data)
+      .then(result => {
+
+        commit('fetchAllProducts', result.results.sort((a, b) => b.weight - a.weight))
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  },
 
 }
 
+const mutations = {
+  createProductMessage:(state,productmessage)=>(state.productmessage = productmessage),
+  updateProductMessage:(state,updateproductmessage)=>(state.updateproductmessage = updateproductmessage),
+  fetchProduct:(state,product) =>(state.product = product),
+  fetchAllProducts:(state,products)=>(state.products = products),
+  ProductById:(state,ProductById)=>(state.ProductById = ProductById),
+  deleteProductById:(state,deleteProductById)=>(state.deleteProductById = deleteProductById),  
+}
 
-
-export default{
-    state,
-    getters,
-    actions,
-    mutations,
+export default {
+  state,
+  getters,
+  actions,
+  mutations,
 }
